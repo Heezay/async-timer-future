5089a8f4fb1fc64703202229e18734bd0377da07
Heezay <den_9912@mail.ru>
2023-04-13 15:22:56 +0600

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,42 @@ use std::{
 // The timer we wrote in the previous section:
 use async_timer_future::TimerFuture;
 
+/// Task executor that receives tasks off of a channel and runs them.
 struct Executor {
     ready_queue: Receiver<Arc<Task>>,
 }
 
+/// `Spawner` spawns new futures onto the task channel.
 #[derive(Clone)]
 struct Spawner {
     task_sender: SyncSender<Arc<Task>>,
 }
 
+/// A future that can reschedule itself to be polled by an `Executor`.
 struct Task {
+    /// In-progress future that should be pushed to completion.
+    ///
+    /// The `Mutex` is not necessary for correctness, since we only have
+    /// one thread executing tasks at once. However, Rust isn't smart
+    /// enough to know that `future` is only mutated from one thread,
+    /// so we need to use the `Mutex` to prove thread-safety. A production
+    /// executor would not need this, and could use `UnsafeCell` instead.
     future: Mutex<Option<BoxFuture<'static, ()>>>,
+    /// Handle to place the task itself back onto the task queue.
     task_sender: SyncSender<Arc<Task>>,
 }
 
 fn new_executor_and_spawner() -> (Executor, Spawner) {
+    // Maximum number of tasks to allow queueing in the channel at once.
+    // This is just to make `sync_channel` happy, and wouldn't be present in
+    // a real executor.
     const MAX_QUEUED_TASKS: usize = 10_000;
     let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
     (Executor { ready_queue }, Spawner { task_sender })
 }
 
 impl Spawner {
+    /// This method will take a future type, box it, and create a new Arc<Task> with it inside which can be enqueued onto the executor.
     fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
         let future = future.boxed();
         let task = Arc::new(Task {
@@ -44,6 +59,8 @@ impl Spawner {
 }
 
 impl ArcWake for Task {
+    // Implement `wake` by sending this task back onto the task channel
+    // so that it will be polled again by the executor.
     fn wake_by_ref(arc_self: &Arc<Self>) {
         let cloned = arc_self.clone();
         arc_self
@@ -56,11 +73,20 @@ impl ArcWake for Task {
 impl Executor {
     fn run(&self) {
         while let Ok(task) = self.ready_queue.recv() {
+            // Take the future, and if it has not yet completed (is still Some),
+            // poll it in an attempt to complete it.
             let mut future_slot = task.future.lock().unwrap();
             if let Some(mut future) = future_slot.take() {
+                // Create a `LocalWaker` from the task itself
                 let waker = waker_ref(&task);
                 let context = &mut Context::from_waker(&waker);
+                // `BoxFuture<T>` is a type alias for
+                // `Pin<Box<dyn Future<Output = T> + Send + 'static>>`.
+                // We can get a `Pin<&mut dyn Future + Send + 'static>`
+                // from it by calling the `Pin::as_mut` method.
                 if future.as_mut().poll(context).is_pending() {
+                    // We're not done processing the future, so put it
+                    // back in its task to be run again in the future.
                     *future_slot = Some(future);
                 }
             }
@@ -71,13 +97,19 @@ impl Executor {
 fn main() {
     let (executor, spawner) = new_executor_and_spawner();
 
+    // Spawn a task to print before and after waiting on a timer.
     spawner.spawn(async {
         println!("howdy!");
+        // Wait for our timer future to complete after two seconds.
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("done!");
     });
 
+    // Drop the spawner so that our executor knows it is finished and won't
+    // receive more incoming tasks to run.
     drop(spawner);
 
+    // Run the executor until the task queue is empty.
+    // This will print "howdy!", pause, and then print "done!".
     executor.run();
 }
